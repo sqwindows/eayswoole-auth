0a668125f30cc31a76cfa658ad55179de7609219
sqwindows <sqwindows@qq.com>
2019-11-21 15:32:32 +0800

    0.0.1

--- README.md
+++ README.md
@@ -1,11 +1,99 @@
 ## 简介
-EasySwooleAuth 权限验证，计划开发中。
+EasySwooleAuth RABC版 权限验证。
 ## 安装
 ```bash
 composer require crazycater/easyswoole-auth
 ```
 ## 使用方法
+
+1.控制器调用方法
+```php
+     $Auth = new \CrazyCater\EasySwooleAuth\Auth([
+          //'auth_on' => true,                // 认证开关
+          'session' => $this->session(),    // 认证方式:false为实时认证,传SessionDriver则为session认证。
+          'allow_userids' => [10000],  //不用检测权限的用户编号
+          'allow_urls' => ['Admin/V1/SystemAuthMenu/tree'], //不用检测权限的URL
+          //'mysql_prefix' => 'cater_',          //表名前缀
+          //'system_auth_group' => 'system_auth_group',        // 用户组数据表名
+          //'system_auth_group_access' => 'system_auth_group_access', // 用户-用户组关系表
+          //'system_auth_menu' => 'system_auth_menu',         // 权限规则表
+          //'system_user' => 'system_user',            // 用户信息表
+          //'user_field_name' => 'system_user_id',                // 用户表ID字段名
+          //'group_field_name' => 'system_auth_group_id',    //用户组表组关联字段命
+          //'rules_field_name' => 'menus', //system_auth_group表中存储规则的字段名
+          //'menu_pk_field_name' => 'system_auth_menu_id',
+          
+     ]);
+     //默认验证方法
+     $check1 = $Auth->check($user_id, $authUrl, $this->request()->getRequestParam());
+     var_dump($check1);
+    
+    //可自定义默认验证地址方式
+     $check2 = $Auth->check($user_id, $authUrl, $this->request()->getRequestParam(), function ($info) {
+           return $info['module'] . '/' . $info['url'];
+         // return $info['module'] . '/'. $info['version'] .'/' . $info['url'];//默认验证地址方式
+      });
+      var_dump($check2);
+         
 ```
+2.需要创建一下三张数据表
  
+```sql
+ 
+ CREATE TABLE IF NOT EXISTS `cater_system_auth_group` (
+   `system_auth_group_id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '用户编号',
+   `title` varchar(64) NOT NULL COMMENT '分组名称',
+   `description` varchar(64) NOT NULL DEFAULT '' COMMENT '分组描述',
+   `module` varchar(32) NOT NULL DEFAULT 'Admin' COMMENT '隶属模型',
+   `version` varchar(32) NOT NULL DEFAULT 'V1' COMMENT '版本名称',
+   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '分组状态',
+   `menus` text NOT NULL DEFAULT '' COMMENT '规则编号',
+   `create_time` int(10) NOT NULL DEFAULT 0 COMMENT '创建时间',
+   `update_time` int(10) NOT NULL DEFAULT 0 COMMENT '更新时间',
+   INDEX `title` (`title`),
+   INDEX `module` (`module`),
+   INDEX `status` (`status`),
+   INDEX `version` (`version`)
+ )
+ ENGINE = INNODB DEFAULT COLLATE = 'utf8_general_ci' COMMENT = '权限分组表';
+ 
+ 
+ CREATE TABLE IF NOT EXISTS `cater_system_auth_group_access` (
+   `system_auth_group_access_id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '自动编号',
+   `system_user_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '用户编号',
+   `system_auth_group_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '分组编号',
+   `create_time` int(10) NOT NULL DEFAULT 0 COMMENT '创建时间',
+   `update_time` int(10) NOT NULL DEFAULT 0 COMMENT '更新时间',
+   INDEX `system_user_id` (`system_user_id`),
+   INDEX `system_auth_group_id` (`system_auth_group_id`)
+ )
+ ENGINE = INNODB DEFAULT COLLATE = 'utf8_general_ci' COMMENT = '权限分组表';
+ 
+ 
+ CREATE TABLE IF NOT EXISTS `cater_system_auth_menu` (
+   `system_auth_menu_id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '菜单编号',
+   `title` varchar(64) NOT NULL COMMENT '菜单名称',
+   `module` varchar(32) NOT NULL DEFAULT 'Admin' COMMENT '隶属模型',
+   `version` varchar(32) NOT NULL DEFAULT 'V1' COMMENT '隶属版本',
+   `url` varchar(200) NOT NULL COMMENT '菜单地址',
+   `icon` varchar(200) NOT NULL COMMENT '菜单图标',
+   `pid` int(10) NOT NULL DEFAULT 0 COMMENT '父级编号',
+   `sort` int(10) NOT NULL DEFAULT 0 COMMENT '菜单排序',
+   `is_hide` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否隐藏',
+   `is_dev` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否隐藏',
+   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否显示',
+   `create_time` int(10) NOT NULL DEFAULT 0 COMMENT '创建时间',
+   `update_time` int(10) NOT NULL DEFAULT 0 COMMENT '更新时间',
+   INDEX `title` (`title`),
+   INDEX `module` (`module`),
+   INDEX `version` (`version`),
+   INDEX `url` (`url`),
+   INDEX `pid` (`pid`),
+   INDEX `sort` (`sort`),
+   INDEX `is_hide` (`is_hide`),
+   INDEX `is_dev` (`is_dev`),
+   INDEX `status` (`status`)
+ )
+ ENGINE = INNODB DEFAULT COLLATE = 'utf8_general_ci' COMMENT = '规则菜单';
 
 ```
--- src/Auth.php
+++ src/Auth.php
@@ -7,18 +7,157 @@
 
 namespace CrazyCater\EasySwooleAuth;
 
+use CrazyCater\EasySwooleAuth\Model\SystemAuthGroup;
+use CrazyCater\EasySwooleAuth\Model\SystemAuthGroupAccess;
+use CrazyCater\EasySwooleAuth\Model\SystemAuthMenu;
 
 class Auth
 {
 
-    public function __construct()
+    protected $config = [
+        'auth_on' => true,                // 认证开关
+        'session' => false,                   // 认证方式:false为实时认证,传SessionDriver则为session认证。
+        'mysql_prefix' => 'cater_',          //表名前缀
+        'system_auth_group' => 'system_auth_group',        // 用户组数据表名
+        'system_auth_group_access' => 'system_auth_group_access', // 用户-用户组关系表
+        'system_auth_menu' => 'system_auth_menu',         // 权限规则表
+        'system_user' => 'system_user',            // 用户信息表
+        'user_field_name' => 'system_user_id',                // 用户表ID字段名
+        'group_field_name' => 'system_auth_group_id',    //用户组表组关联字段命
+        'rules_field_name' => 'menus', //system_auth_group表中存储规则的字段名
+        'menu_pk_field_name' => 'system_auth_menu_id',
+        'allow_userids' => [],  //不用检测权限的用户编号
+        'allow_urls' => [], //不用检测权限的URL
+    ];
+
+    public function __construct($config = [])
     {
+        if (!empty($config)) {
+            $this->config = array_merge($this->config, $config);
+        }
+        $this->prefix = !empty($this->config['mysql_prefix']) ? $this->config['mysql_prefix'] : \EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL.prefix');
+    }
+
+    /**
+     * @param string system_user_id 用户ID
+     * @param string url url地址
+     * @param  string urlParams  url参数
+     * @param  callable urlTargetType url串接验证方式
+     * @return bool
+     * @author crazyCater
+     */
+    public function check(int $system_user_id = 0, string $url = '', array $urlParams = [], callable $urlTargetType = null)
+    {
+        if (!$this->config['auth_on']) {
+            return true;
+        }
+        if (in_array($system_user_id, (array)$this->config['allow_userids'])) {
+            return true;
+        }
+        if (in_array($url, (array)$this->config['allow_urls'])) {
+            return true;
+        }
+        $this->urlTargetType = $urlTargetType ?? null;
+        $this->system_user_id = abs(intval($system_user_id));
+        if ($this->system_user_id <= 0 || !$url) {
+            return false;
+        }
+        $this->menuLists = null;
+        $cacheSessionKey = '__UserAuthLists__' . $this->system_user_id;
+        $isSession = ($this->config['session'] && gettype($this->config['session']) == 'object') ? true : false;
+        if ($isSession) {
+            $this->session = $this->config['session'];
+            $this->menuLists = $this->session->get($cacheSessionKey);
+        }
+        if (!$this->menuLists) {
+            $this->menuLists = $this->getUserMenuLists($this->getUserMenuIds($this->system_user_id));
+            if ($isSession)
+                $this->session->set($cacheSessionKey, $this->menuLists);
+        }
+        if (!$this->menuLists) {
+            return false;
+        }
+        $this->url = $url;
+        $this->urlParams = $urlParams ? array_keys($urlParams) : [];
+        return $this->_checkUrl();
+    }
+
+
+    protected function getUserMenuLists($menuId = [])
+    {
+        $SystemAuthMenu = new SystemAuthMenu($this->config);
+        $lists = $SystemAuthMenu->create()
+            ->where($this->config['menu_pk_field_name'], $menuId, 'in')
+            ->where('url', '', '!=')
+            ->where('status', 1)
+            ->field('module,version,url,params')
+            ->select();
+        $menuLists = [];
+        if ($lists) {
+            foreach ($lists as $info) {
+                $menuLists[] = [
+                    'url' => $this->urlTargetType ? call_user_func($this->urlTargetType, $info) : $info['module'] . '/' . $info['version'] . '/' . $info['url'],
+                    'params' => array_filter(explode(',', $info['params']))
+                ];
+            }
+            print_r($menuLists);
+        }
+        return $menuLists;
+    }
+
+    protected function getUserMenuIds($system_user_id = 0)
+    {
+        if ($system_user_id <= 0) {
+            return [];
+        }
+        $SystemAuthGroupAccess = new SystemAuthGroupAccess($this->config);
+        $lists = $SystemAuthGroupAccess->alias('groupAccess')
+            ->join($this->prefix . 'system_auth_group AS `group`', 'group.' . $this->config['group_field_name'] . ' = groupAccess.' . $this->config['group_field_name'])
+            ->where('groupAccess.' . $this->config['user_field_name'], $system_user_id)
+            ->field('group.' . $this->config['rules_field_name'])
+            ->select();
+        $rules = [];
+        if ($lists) {
+            foreach ($lists as $key => $val) {
+                $menus = array_filter(explode(',', $val[$this->config['rules_field_name']]));
+                if ($menus && is_array($menus)) {
+                    foreach ($menus as $v) {
+                        $rules[] = $v;
+                    }
+                }
+            }
+        }
+        return $rules ? array_values(array_unique($rules)) : [];
+    }
+
+    private function _checkParams($params = []): bool
+    {
+        $params = array_filter($params);
+        if (!$params) {
+            return true;
+        }
+        $res = false;
+        foreach ($this->urlParams as $key => $val) {
+            if (in_array($val, $params)) {
+                $res = true;
+            }
+        }
+        return $res;
 
     }
 
-    public function check()
+    private function _checkUrl(): bool
     {
-        return true;
+        $res = false;
+        foreach ($this->menuLists as $key => $val) {
+            if ($this->url === $val['url']) {
+                $res = true;
+                if ($this->_checkParams($val['params']) === false) {
+                    $res = false;
+                }
+            }
+        }
+        return $res;
     }
 
 }
